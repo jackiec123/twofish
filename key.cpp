7886e01c0133e69d7dae4167a01ef9f012ba5bfa
jackiec123 <jackiecreitz@gmail.com>
2017-01-31 12:05:28 -0500

    Split key into 2k 32 bit words

--- key.cpp
+++ key.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 
 using namespace std;
 
@@ -16,7 +17,8 @@ int main(){
 	}
 
 	cout<<"\n THE KEY";
-	//generate 128 bit key, N=128,192, 256 can set this in the future, for now its 128
+	//generate 128 bit key, N=128,192, 256. can set this in the future, for now its 128
+	int N = 128; //key size
 	int key[128];
 	for (int n = 0; n<128; n++){
 	  int i  = rand()%2;
@@ -25,9 +27,36 @@ int main(){
 	}
 
 	//k = N/64, where N=128
-	int k = 128/64;
+	int k = N/64;
 	cout<<"\n";
-	cout<<k;
+	//cout<<k;
+	cout<<"\n";
+
+	//Divide message into 2k words of 32 bits, so 4 words
+	int M0[32]; //bits 0-31 from key array
+	int M1[32]; //bits 32-63
+	int M2[32];
+	int M3[32];
+	//cout<< pow (2.0, 0);
+	
+	for (int i = 0; i<32; i++){
+		M0[i] = key[i];
+		//cout<<M0[i];
+	}
+
+	for (int i =32; i<64; i++){
+		M1[i]=key[i];
+	}
+	for (int i = 64; i<96; i++){
+		M2[i] = key[i];
+	}
+	for (int i = 97; i<128; i++){
+		M3[i] = key[i];
+		//cout<<M3[i];
+	}
+	
+
+
 
 	
 }
